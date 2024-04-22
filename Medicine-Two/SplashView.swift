17d298f2882b048789231431d6e45442dcc2d095
Kaif Pathan <kaifakpathan@gmail.com>
2024-04-22 19:08:43 +0100

    SplashView.swift

---
+++ Medicine-Two/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  Medicine-Two
+//
+//  Created by Kaif Ali Khan Pathan on 22/04/2024.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
