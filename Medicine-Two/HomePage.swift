9eeafcad4f5e3cd9fa84c09cbe3df5a1437379e8
KaifxDev <kaifakpathan@gmail.com>
2024-04-22 19:35:43 +0100

    Created homepage view

---
+++ Medicine-Two/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  HomePage.swift
+//  Medicine-Two
+//
+//  Created by Kaif Ali Khan Pathan on 22/04/2024.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePage()
+}
